baa921b5e3b824a2494eb3acf4c8aa00888e6344
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-03 10:58:09 +0900

    improve: add test to get_instagram.py

--- get_instagram.py
+++ get_instagram.py
@@ -1,26 +1,40 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# instagram.py
+# get_instagram.py
 
-import requests
+import os
+import sha
+
 from instagram.client import InstagramAPI
 
 import load_yaml
+import download_img
 
 
 def get_instagram(key):
     api = InstagramAPI(client_id=key['client_id'],
                        client_secret=key['client_secret'])
     popular_media = api.media_popular(count=20)
+
+    urls = []
     for media in popular_media:
-        img_url = media.images['standard_resolution'].url
-        print img_url
+        url = media.images['standard_resolution'].url
+        urls.append(url)
+    return urls
 
 
-def main():
+def test_get_instagram():
     key = load_yaml.load_yaml(sns='instagram')[0]
-    data = get_instagram(key=key)
+    urls = get_instagram(key=key)
+
+    # make directory to save imgs
+    if not os.path.exists('img'):
+        os.mkdir('img')
+
+    for url in urls:
+        filename = 'img/' + sha.sha(url).hexdigest()
+        download_img.download_img(url=url, filename=filename)
 
 
 if __name__ == '__main__':
-    main()
+    test_get_instagram()
